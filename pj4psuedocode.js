183e40efa2cc6eb608f57463b8b4c5407b9e8057
Andrew Alexander <aarubesa@gmail.com>
2019-11-20 17:45:26 -0500

    commit of final psuedo code after scope approval

--- pj4psuedocode.js
+++ pj4psuedocode.js
@@ -4,10 +4,11 @@
 
 //1. Document ready
 //2. Set up namespace qrApp:
-const qrApp = {};
 //3. Init to start the function
 //4. Pull user data from form with following parameters:
 //a) User's portfolio URL.
-//b) Buttons for user to select what format they want their qr code generated as (JPEG, PNG, SVG).
-//5. Make AJAX request to goqr.me to endpoint: https://api.qrserver.com/v1/create-qr-code/?data=
-//6. Generate the users qr code to the page based on users parameters.
+//b) User's width.
+//c) User's background color.
+//5. Submission button for user to generate qrcode along with download button to allow user to download their qrcode locally.
+//6. Make AJAX request to happi.dev to endpoint: https://api.happi.dev/v1/qrcode
+//7. Generate the users qr code to the page based on users parameters.
